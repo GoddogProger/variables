e3b94e9430c58348250189ce3da3d081c3256063
GoddogProger <62662100+GoddogProger@users.noreply.github.com>
2020-03-27 11:30:35 +0300

    Create variables.cs

---
+++ variables.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace CAtset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 24;
+            string name = "Денис";
+            string surname = "Вадисов";
+            char favoriteletter = 'A';
+            bool isCar = true;
+            int age = 24;
+            int yearBirth = 1996;
+            float weight = 79.7f;
+            float height = 157.7f;
+        }
+    }
+}
